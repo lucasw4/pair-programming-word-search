240f795c722d293d336f28afee482195451ccaa2
Lucas Sahota <lucaswade4@gmail.com>
2022-10-26 13:38:06 -0700

    Added changes

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const verticalToHorizontal = function(array) {
+  const results = [];
+  for (let i = 0; i < array[0].length; i++) {
+    const col = [];
+    for (let j = 0; j < array.length; j++) {
+      col.push(array[j][i])
     }
-}
+    results.push(col)
+  }
+  return results
+  };
+const wordSearch = (letters, word) => { 
+  if(letters.length === 0) return false
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    const verticalJoin = verticalToHorizontal(letters).map(ls => ls.join(''))
+    // console.log(horizontalJoin)
+    for (item of horizontalJoin) {
+        if (item.includes(word)) {
+          return true
+        } 
+    }
+    for (item2 of verticalJoin) {
+      if(item2.includes(word))
+      return true;
+    }
+    verticalToHorizontal(letters)
+    return false
+  }
 
 module.exports = wordSearch
